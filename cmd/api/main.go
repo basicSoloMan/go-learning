dd561036919a53ac8814c6b6e0eade1409294ad3
Marius <mariusjansevanrensburg1@gmail.com>
2025-01-14 20:42:44 +0200

    Build mux and add health checks

---
+++ cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "log"
+
+func main() {
+	cfg := config{
+		addr: "localhost:8080",
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module go-learning
+
+go 1.23.4
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect
