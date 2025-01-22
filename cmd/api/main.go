6f22176490826abbb6bb9f386298e8d560d21ef9
Marius <mariusjansevanrensburg1@gmail.com>
2025-01-22 20:13:31 +0200

    Add database interaction

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	db2 "go-learning/internal/db"
 	"go-learning/internal/env"
 	store2 "go-learning/internal/store"
 	"log"
@@ -9,9 +10,23 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", "localhost:8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store2.NewStorage(nil)
+	db, err := db2.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("Database Connection Pool Established")
+	
+	store := store2.NewStorage(db)
 
 	app := &application{
 		config: cfg,
